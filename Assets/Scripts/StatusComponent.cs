9e8725633b0edb10e5ae484f99fa7723799bdef4
GiuVol <g.volpe40@studenti.uniba.it>
2022-05-28 21:54:43 +0200

    Defined IStatusable interface and StatusComponent

---
+++ Assets/Scripts/StatusComponent.cs
@@ -0,0 +1,216 @@
+using System.Collections;
+using UnityEngine;
+
+public class StatusComponent : MonoBehaviour
+{
+    private const float MinBlindnessLevelDecrementSpeed = .5f;
+
+    #region Blindness
+
+    /// <summary>
+    /// The current blindness level of the character with this <c>StatusComponent</c> attached.
+    /// </summary>
+    public float CurrentBlindnesslevel { get; private set; }
+
+    /// <summary>
+    /// The max blindness level of the character with this <c>StatusComponent</c> attached. 
+    /// When the <c>CurrentBlindnessLevel</c> reaches this value, the character will be blinded.
+    /// </summary>
+    public float MaxBlindnesslevel { get; private set; }
+
+    /// <summary>
+    /// This value determines how long the character remains blinded.
+    /// </summary>
+    public float BlindnessDuration { get; private set; }
+
+    /// <summary>
+    /// This value determines how long it takes to increase the <c>CurrentBlindnesslevel</c> again 
+    /// after the <c>CurrentBlindnesslevel</c> reaches the 0.
+    /// </summary>
+    public float BlindnessCooldownTime { get; private set; }
+
+    /// <summary>
+    /// The greater this value is, the more it will take to increase the <c>CurrentBlindnessLevel</c>.
+    /// This value should be clamped between 0 and 1.
+    /// </summary>
+    public float BlindnessResistance { get; private set; }
+
+    /// <summary>
+    /// The greater this value is, the faster the <c>CurrentBlindnessLevel</c> will decrease.
+    /// </summary>
+    public float BlindnessLevelDecrementSpeed { get; private set; }
+
+    /// <summary>
+    /// This value represents whether the character is blinded or not.
+    /// </summary>
+    public bool IsBlinded { get; private set; }
+
+    /// <summary>
+    /// This value represents whether the character can be blinded or not.
+    /// </summary>
+    public bool CanBeBlinded { get; private set; }
+
+    #endregion
+
+    #region Corrosion
+
+    /// <summary>
+    /// The corrosion time left.
+    /// </summary>
+    public float CorrosionTimeLeft { get; private set; }
+
+    /// <summary>
+    /// The max corrosion time that the character can reach.
+    /// </summary>
+    public float MaxCorrosionTime { get; private set; }
+
+    /// <summary>
+    /// The greater this value is, the less the character will be damaged from corrosion.
+    /// This value should be clamped between 0 and 1.
+    /// </summary>
+    public float CorrosionResistance { get; private set; }
+
+    /// <summary>
+    /// This value represents whether the character is corroded or not.
+    /// </summary>
+    public bool IsCorroded { get; private set; }
+
+    #endregion
+
+    private bool _initialized;
+
+    void Start()
+    {
+        CurrentBlindnesslevel = 0;
+        IsBlinded = false;
+        CanBeBlinded = true;
+
+        CorrosionTimeLeft = 0;
+        IsCorroded = false;
+
+        _initialized = false;
+    }
+
+    /// <summary>
+    /// This method is used to setup the <c>StatusComponent</c> with the desired values.
+    /// </summary>
+    /// <param name="maxBlindnesslevel">
+    /// The desired value for <c>MaxBlindnesslevel</c>
+    /// </param>
+    /// <param name="blindnessDuration">
+    /// The desired value for <c>BlindnessDuration</c>
+    /// </param>
+    /// <param name="blindnessCooldownTime">
+    /// The desired value for <c>BlindnessCooldownTime</c>
+    /// </param>
+    /// <param name="blindnessResistance">
+    /// The desired value for <c>BlindnessResistance</c>
+    /// pre: this value should be clamped between 0 and 1.
+    /// </param>
+    /// <param name="blindnessLevelDecrementSpeed">
+    /// The desired value for <c>BlindnessLevelDecrementSpeed</c>
+    /// </param>
+    /// <param name="maxCorrosionTime">
+    /// The desired value for <c>MaxCorrosionTime</c>
+    /// </param>
+    /// <param name="corrosionResistance">
+    /// The desired value for <c>CorrosionResistance</c>
+    /// pre: this value should be clamped between 0 and 1.
+    /// </param>
+    public void Setup(float maxBlindnesslevel, float blindnessDuration, 
+                      float blindnessCooldownTime, float blindnessResistance, 
+                      float blindnessLevelDecrementSpeed, float maxCorrosionTime, 
+                      float corrosionResistance)
+    {
+        MaxBlindnesslevel = maxBlindnesslevel;
+        BlindnessDuration = Mathf.Min(blindnessDuration, maxBlindnesslevel / blindnessLevelDecrementSpeed);
+        BlindnessCooldownTime = blindnessCooldownTime;
+        BlindnessResistance = blindnessResistance;
+        BlindnessLevelDecrementSpeed = blindnessLevelDecrementSpeed;
+
+        MaxCorrosionTime = maxCorrosionTime;
+        CorrosionResistance = corrosionResistance;
+
+        _initialized = true;
+    }
+
+    protected void FixedUpdate()
+    {
+        if (!_initialized)
+        {
+            return;
+        }
+
+        if (CurrentBlindnesslevel >= MaxBlindnesslevel && !IsBlinded)
+        {
+            StartCoroutine(InflictBlindness());
+        }
+
+        CurrentBlindnesslevel -= BlindnessLevelDecrementSpeed * Time.fixedDeltaTime;
+        CurrentBlindnesslevel = Mathf.Clamp(CurrentBlindnesslevel, 0, MaxBlindnesslevel);
+
+        CorrosionTimeLeft -= Time.fixedDeltaTime;
+        CorrosionTimeLeft = Mathf.Clamp(CorrosionTimeLeft, 0, MaxCorrosionTime);
+
+        IsCorroded = CorrosionTimeLeft > 0;
+    }
+
+    /// <summary>
+    /// The IEnumerator used to inflict blindness when the <c>CurrentBlindnesslevel</c> reaches the <c>MaxBlindnesslevel</c>.
+    /// This IEnumerator should be passed to the <c>StartCoroutine()</c> method, it automatically sets the blindness for the 
+    /// correct time.
+    /// </summary>
+    private IEnumerator InflictBlindness()
+    {
+        IsBlinded = true;
+        CanBeBlinded = false;
+
+        float actualBlindnessLevelDecrementSpeed = Mathf.Max(BlindnessLevelDecrementSpeed, MinBlindnessLevelDecrementSpeed);
+        float timeToWait = 
+            Mathf.Min(BlindnessDuration, 
+                      MaxBlindnesslevel / actualBlindnessLevelDecrementSpeed);
+
+        yield return new WaitForSeconds(timeToWait);
+
+        IsBlinded = false;
+
+        yield return new WaitUntil(() => CurrentBlindnesslevel == 0);
+
+        yield return new WaitForSeconds(BlindnessCooldownTime);
+
+        CanBeBlinded = true;
+    }
+
+    /// <summary>
+    /// This method is used to increase the <c>CurrentBlindnesslevel</c> of the character.
+    /// </summary>
+    /// <param name="increment">The desired increment</param>
+    public void IncreaseBlindnessLevel(float increment)
+    {
+        if (CanBeBlinded == false)
+        {
+            return;
+        }
+
+        increment = Mathf.Max(increment, 0);
+        float actualBlindnessResistance = Mathf.Clamp01(BlindnessResistance);
+
+        increment = (1 - actualBlindnessResistance) * increment;
+
+        CurrentBlindnesslevel = Mathf.Min(MaxBlindnesslevel, CurrentBlindnesslevel + increment);
+    }
+
+    /// <summary>
+    /// This method is used to increase the <c>CorrosionTimeLeft</c> of the character.
+    /// </summary>
+    /// <param name="increment">The desired increment</param>
+    public void IncreaseCorrosionTime(float increment)
+    {
+        if (CorrosionTimeLeft > 0)
+        {
+            return;
+        }
+
+        CorrosionTimeLeft = Mathf.Min(MaxCorrosionTime, CorrosionTimeLeft + increment);
+    }
+}
