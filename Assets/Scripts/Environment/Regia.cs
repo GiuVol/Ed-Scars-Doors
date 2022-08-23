79256fef7916357558cfbab1cba7f90504326d82
GiuVol <g.volpe40@studenti.uniba.it>
2022-08-23 12:55:48 +0200

    Improved regia

--- Assets/Scripts/Environment/Regia.cs
+++ Assets/Scripts/Environment/Regia.cs
@@ -15,7 +15,7 @@ public class Regia : MonoBehaviour
         private float _cameraSize;
 
         [SerializeField]
-        private float _cameraOffset;
+        private Vector2 _cameraOffset;
 
         public float XPosition
         {
@@ -33,7 +33,7 @@ public class Regia : MonoBehaviour
             }
         }
 
-        public float CameraOffset
+        public Vector2 CameraOffset
         {
             get
             {
@@ -59,29 +59,44 @@ public class Regia : MonoBehaviour
     [SerializeField]
     private List<CameraPresetValue> _cameraValues;
 
-    private Camera _camera;
+    private CameraController _cameraController;
     private Vector3 _lastCameraPosition;
+
+    private PlayerController _playerController;
 
     private bool _initialized;
 
-    private void Start()
+    private void Update()
     {
-        StartRegia(Camera.main);
-    }
-
-    public void StartRegia(Camera camera)
-    {
-        if (camera == null)
+        if (_initialized)
         {
             return;
         }
 
-        _camera = camera;
-        _lastCameraPosition = _camera.transform.position;
+        CameraController cc = GameObject.FindObjectOfType<CameraController>();
+        PlayerController pc = GameObject.FindObjectOfType<PlayerController>();
+
+        if (cc != null && pc != null)
+        {
+            StartRegia(cc, pc);
+        }
+    }
+
+    public void StartRegia(CameraController cameraController, PlayerController playerController)
+    {
+        if (cameraController == null)
+        {
+            return;
+        }
+
+        _cameraController = cameraController;
+        _lastCameraPosition = _cameraController.transform.position;
+
+        _playerController = playerController;
 
         if (_background != null)
         {
-            _background.Initialize(_camera.transform.position);
+            _background.Initialize(_cameraController.transform.position);
         }
 
         if (_cameraValues != null && _cameraValues.Count > 0)
@@ -106,13 +121,13 @@ public class Regia : MonoBehaviour
 
         if (_cameraValues != null && _cameraValues.Count > 0)
         {
-
+            HandleCamera();
         }
     }
 
     private void HandleBackground()
     {
-        Vector3 deltaPosition = _camera.transform.position - _lastCameraPosition;
+        Vector3 deltaPosition = _cameraController.transform.position - _lastCameraPosition;
 
         foreach (ParallaxBackground.BackgroundLayer layer in _background.Layers)
         {
@@ -123,29 +138,77 @@ public class Regia : MonoBehaviour
 
             if (layer.LoopHorizontal)
             {
-                if (Mathf.Abs(_camera.transform.position.x - (layer.GameObject.transform.position.x + layer.PositionOffset.x)) 
+                if (Mathf.Abs(_cameraController.transform.position.x - (layer.GameObject.transform.position.x + layer.PositionOffset.x)) 
                     >= layer.TextureUnitSizeX)
                 {
-                    float offsetPositionX = (_camera.transform.position.x - layer.GameObject.transform.position.x) %
+                    float offsetPositionX = (_cameraController.transform.position.x - layer.GameObject.transform.position.x) %
                         layer.TextureUnitSizeX;
                     layer.GameObject.transform.position =
-                        new Vector3(_camera.transform.position.x + offsetPositionX, layer.GameObject.transform.position.y);
+                        new Vector3(_cameraController.transform.position.x + offsetPositionX, layer.GameObject.transform.position.y);
                 }
             }
 
             if (layer.LoopVertical)
             {
-                if (Mathf.Abs(_camera.transform.position.y - (layer.GameObject.transform.position.y + layer.PositionOffset.y)) 
+                if (Mathf.Abs(_cameraController.transform.position.y - (layer.GameObject.transform.position.y + layer.PositionOffset.y)) 
                     >= layer.TextureUnitSizeY)
                 {
-                    float offsetPositionY = (_camera.transform.position.y - layer.GameObject.transform.position.y) %
+                    float offsetPositionY = (_cameraController.transform.position.y - layer.GameObject.transform.position.y) %
                         layer.TextureUnitSizeY;
                     layer.GameObject.transform.position =
-                        new Vector3(layer.GameObject.transform.position.x, _camera.transform.position.y + offsetPositionY);
+                        new Vector3(layer.GameObject.transform.position.x, _cameraController.transform.position.y + offsetPositionY);
                 }
             }
         }
 
-        _lastCameraPosition = _camera.transform.position;
+        _lastCameraPosition = _cameraController.transform.position;
+    }
+
+    private void HandleCamera()
+    {
+        if (_cameraValues == null || _cameraValues.Count <= 0)
+        {
+            return;
+        }
+
+        float playerXPosition = _playerController.transform.position.x;
+
+        float cameraSize = 10;
+        Vector2 cameraOffset = Vector2.zero;
+
+        if (playerXPosition <= _cameraValues[0].XPosition)
+        {
+            cameraSize = _cameraValues[0].CameraSize;
+            cameraOffset = _cameraValues[0].CameraOffset;
+        } else if (playerXPosition >= _cameraValues[_cameraValues.Count - 1].XPosition)
+        {
+            cameraSize = _cameraValues[_cameraValues.Count - 1].CameraSize;
+            cameraOffset = _cameraValues[_cameraValues.Count - 1].CameraOffset;
+        } else
+        {
+            for (int i = 0; i < _cameraValues.Count - 1; i++)
+            {
+                CameraPresetValue current = _cameraValues[i];
+                CameraPresetValue next = _cameraValues[i + 1];
+
+                if (playerXPosition >= current.XPosition && playerXPosition <= next.XPosition)
+                {
+                    float relativePosition = playerXPosition - current.XPosition;
+                    float interval = Mathf.Abs(next.XPosition - current.XPosition);
+
+                    float lerpFactor = relativePosition / interval;
+
+                    cameraSize = 
+                        Mathf.Lerp(current.CameraSize, next.CameraSize, lerpFactor);
+                    cameraOffset =
+                        Vector2.Lerp(current.CameraOffset, next.CameraOffset, lerpFactor);
+
+                    break;
+                }
+            }
+        }
+
+        _cameraController.CameraComponent.orthographicSize = cameraSize;
+        _cameraController.PositionOffset = cameraOffset;
     }
 }
