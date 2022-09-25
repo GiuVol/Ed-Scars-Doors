b3def5454cf40c0e4993398971d802e0ba2b00e2
GiuVol <g.volpe40@studenti.uniba.it>
2022-09-25 19:32:52 +0200

    Refined MainMenu

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -5,6 +5,96 @@ using UnityEngine.UI;
 
 public class GameOverMenu : MonoBehaviour
 {
+    #region Buttons Logic
+
+    [SerializeField]
+    private List<Button> _menuButtons;
+
+    private List<Button> MenuButtons
+    {
+        get
+        {
+            if (_menuButtons == null)
+            {
+                _menuButtons = new List<Button>();
+            }
+
+            return _menuButtons;
+        }
+    }
+
+    private int _selectedButtonIndex;
+
+    public int SelectedButtonIndex
+    {
+        get
+        {
+            if (MenuButtons.Count <= 0)
+            {
+                return 0;
+            }
+
+            return Mathf.Clamp(_selectedButtonIndex, 1, MenuButtons.Count);
+        }
+
+        set
+        {
+            if (MenuButtons.Count <= 0)
+            {
+                _selectedButtonIndex = 0;
+                return;
+            }
+
+            int oldValue = _selectedButtonIndex;
+            _selectedButtonIndex = Mathf.Clamp(value, 1, MenuButtons.Count);
+
+            if (_selectedButtonIndex == oldValue)
+            {
+                return;
+            }
+
+            for (int i = 1; i <= MenuButtons.Count; i++)
+            {
+                Button currentButton = _menuButtons[i - 1];
+
+                if (currentButton == null)
+                {
+                    continue;
+                }
+
+                TextMeshProUGUI currentButtonText = currentButton.GetComponentInChildren<TextMeshProUGUI>();
+
+                Color selectedColor = (_selectedButtonIndex == i) ? _enabledTextColor : _disabledTextColor;
+
+                if (currentButtonText != null)
+                {
+                    currentButtonText.color = selectedColor;
+                }
+            }
+        }
+    }
+
+    [SerializeField]
+    private Color _enabledTextColor;
+
+    [SerializeField]
+    private Color _disabledTextColor;
+
+    public Button SelectedButton
+    {
+        get
+        {
+            if (MenuButtons.Count <= 0)
+            {
+                return null;
+            }
+
+            return MenuButtons[SelectedButtonIndex - 1];
+        }
+    }
+
+    #endregion
+    
     /// <summary>
     /// This field stores the button to play again the demo.
     /// </summary>
@@ -65,5 +155,32 @@ public class GameOverMenu : MonoBehaviour
                 #endif
             }
         );
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (SelectedButton != null)
+            {
+                SelectedButtonIndex--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (SelectedButton != null)
+            {
+                SelectedButtonIndex++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (SelectedButton != null)
+            {
+                SelectedButton.onClick.Invoke();
+            }
+        }
     }
 }
--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,29 +1,103 @@
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    /// <summary>
-    /// This field stores the button of the main menu to play the demo.
-    /// </summary>
+    #region Buttons Logic
+
+    [SerializeField]
+    private List<Button> _menuButtons;
+
+    private List<Button> MenuButtons
+    {
+        get
+        {
+            if (_menuButtons == null)
+            {
+                _menuButtons = new List<Button>();
+            }
+
+            return _menuButtons;
+        }
+    }
+
+    private int _selectedButtonIndex;
+
+    public int SelectedButtonIndex
+    {
+        get
+        {
+            if (MenuButtons.Count <= 0)
+            {
+                return 0;
+            }
+
+            return Mathf.Clamp(_selectedButtonIndex, 1, MenuButtons.Count);
+        }
+
+        set
+        {
+            if (MenuButtons.Count <= 0)
+            {
+                _selectedButtonIndex = 0;
+                return;
+            }
+
+            int oldValue = _selectedButtonIndex;
+            _selectedButtonIndex = Mathf.Clamp(value, 1, MenuButtons.Count);
+
+            if (_selectedButtonIndex == oldValue)
+            {
+                return;
+            }
+
+            for (int i = 1; i <= MenuButtons.Count; i++)
+            {
+                Button currentButton = _menuButtons[i - 1];
+
+                if (currentButton == null)
+                {
+                    continue;
+                }
+
+                TextMeshProUGUI currentButtonText = currentButton.GetComponentInChildren<TextMeshProUGUI>();
+
+                Color selectedColor = (_selectedButtonIndex == i) ? _enabledTextColor : _disabledTextColor;
+
+                if (currentButtonText != null)
+                {
+                    currentButtonText.color = selectedColor;
+                }
+            }
+        }
+    }
+
+    [SerializeField]
+    private Color _enabledTextColor;
+
+    [SerializeField]
+    private Color _disabledTextColor;
+    
+    public Button SelectedButton
+    {
+        get
+        {
+            if (MenuButtons.Count <= 0)
+            {
+                return null;
+            }
+
+            return MenuButtons[SelectedButtonIndex - 1];
+        }
+    }
+
+    #endregion
+
     [SerializeField]
     private Button _playDemoButton;
 
-    /// <summary>
-    /// This field stores the button of the main menu to start a new game.
-    /// </summary>
-    [SerializeField]
-    private Button _newGameButton;
-
-    /// <summary>
-    /// This field stores the button of the main menu to continue an existing game.
-    /// </summary>
-    [SerializeField]
-    private Button _continueButton;
-
-    /// <summary>
-    /// This property provides access to <c>_playDemoButton</c> in a controlled way.
-    /// </summary>
     public Button PlayDemoButton
     {
         get
@@ -32,25 +106,35 @@ public class MainMenu : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// This property provides access to <c>_newGameButton</c> in a controlled way.
-    /// </summary>
-    public Button NewGameButton
+    private void Start()
     {
-        get
-        {
-            return _newGameButton;
-        }
+        SelectedButtonIndex = 1;
     }
 
-    /// <summary>
-    /// This property provides access to <c>_continueButton</c> in a controlled way.
-    /// </summary>
-    public Button ContinueButton
+    private void Update()
     {
-        get
+        if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            return _continueButton;
+            if (SelectedButton != null)
+            {
+                SelectedButtonIndex--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (SelectedButton != null)
+            {
+                SelectedButtonIndex++;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if (SelectedButton != null)
+            {
+                SelectedButton.onClick.Invoke();
+            }
         }
     }
 }
