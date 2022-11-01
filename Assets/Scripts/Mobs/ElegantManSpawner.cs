ae14272c836832afc72428bae4836e0181386b00
GiuVol <g.volpe40@studenti.uniba.it>
2022-11-01 19:23:18 +0100

    Added tutorial when elegant man spawns

--- Assets/Scripts/Mobs/ElegantManSpawner.cs
+++ Assets/Scripts/Mobs/ElegantManSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
@@ -11,6 +12,12 @@ public class ElegantManSpawner : EventTrigger
     [SerializeField]
     private PatrolPointsGroup _ppGroup;
 
+    [SerializeField]
+    private bool _startTutorial;
+
+    [SerializeField]
+    private string _promptResourcePath;
+    
     private void Start()
     {
         if (_ppGroup != null)
@@ -37,6 +44,49 @@ public class ElegantManSpawner : EventTrigger
                 _ppGroup.gameObject.SetActive(true);
                 elegantMan.PPGroup = _ppGroup;
             }
+        }
+
+        if (_startTutorial)
+        {
+            yield return new WaitForSeconds(.5f);
+
+            UIPrompt promptResource = Resources.Load<UIPrompt>(_promptResourcePath);
+
+            if (promptResource == null || GameManager.Instance == null)
+            {
+                yield break;
+            }
+
+            if (GameManager.Instance.UI == null || GameManager.Instance.Player == null)
+            {
+                yield break;
+            }
+
+            if (GameManager.Instance.UI.CurrentCanvas == null)
+            {
+                yield break;
+            }
+
+            Canvas canvas = GameManager.Instance.UI.CurrentCanvas;
+
+            GameManager.Instance.UI.PromptIsLoaded = true;
+            player.HasControl = false;
+
+            UIPrompt prompt = Instantiate(promptResource, canvas.transform);
+
+            Time.timeScale = 0;
+
+            List<string> _dialogLines = new List<string>();
+            _dialogLines.Add("Hope: Attento! Quell'essere sembra essere molto pericoloso!");
+            _dialogLines.Add("Hope: Devi evitarlo a tutti i costi!");
+
+            yield return StartCoroutine(prompt.PromptText(_dialogLines,
+                                                          delegate {
+                                                              Time.timeScale = 1;
+                                                              GameManager.Instance.UI.PromptIsLoaded = false;
+                                                              player.HasControl = true;
+                                                          },
+                                                          true));
         }
     }
 }
